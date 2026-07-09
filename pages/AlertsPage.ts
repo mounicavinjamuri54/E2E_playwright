6ac869e04ce1efd9da1628ef58b38bc84a495a7d
Mounicavinjamuri <mounicavinjamuri54@gmail.com>
2026-07-09 21:11:51 +0530

    added reuseable methods } june092026

--- pages/AlertsPage.ts
+++ pages/AlertsPage.ts
@@ -1,23 +1,32 @@
-import { Page, expect } from '@playwright/test';
+import { Page, expect, Locator } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class AlertPage extends BasePage{  ////export(Keyword) :-Now other files can use it 
     // export is a TypeScript/JavaScript keyword used to make a class, function, or variable available to other files..
-
-   // constructor(private page: Page) { } calls this in BasePage
+    readonly heading :  Locator;
+    constructor( page: Page) {
+        super(page); //Because JavaScript automatically calls the parent constructor if you don't define your own constructor.
+     this.heading = this.page.getByRole('heading', { name: 'Alerts' });
+    } //calls this in BasePage
 
     //constructor runs automatically when object got created :-when ---> const alertsPage = new AlertsPage(page);
     //Playwright passes the browser tab(page):---into the constructor.
     // Now every method in the class can use:-----this.page
     //why private:----Because only this class should access the page object can make it as protected also can use child class(inherit concept)).
     //Without private, we must explicitly declare the variable and assign it inside the constructor to keep obj private
-
+     
+     
     async navigateToAlerts() {
 
         await this.page.getByText('Alerts, Frame & Windows', { exact: true }).click();
         await this.page.getByRole('link', { name: 'Alerts', exact: true }).click();
         // await page.getByRole('listitem').filter({ hasText: 'Alerts' }).click();
         await expect(this.page.getByRole('heading', { name: 'Alerts' })).toBeVisible();
+      
+        ///////textContent function is used in BasePage
+        const text = await this.textContent(this.heading);
+        console.log(text);
+        
         await expect(this.page.getByText('Click Button to see alert ')).toBeVisible();
 
     }
@@ -28,7 +37,15 @@ export class AlertPage extends BasePage{  ////export(Keyword) :-Now other files 
             console.log(`Dialog type:', ${dialog.type()}`);
             console.log(`Dialog message:, ${dialog.message()}`);
             dialog.accept();
-        });        await this.page.locator('#alertButton').click();
+        });        
+       // await this.page.locator('#alertButton').click();//can use this one also but without using click function in BasePage
+       
+       //using click option
+       
+       await this.click(this.page.locator('#alertButton'));//click function which is declared in BasePage
+      
+    
     }
 
 }
+
--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -1,4 +1,4 @@
-import {Page} from '@playwright/test';
+import {Page, Locator} from '@playwright/test';
 
 export class BasePage{
 
@@ -8,5 +8,31 @@ export class BasePage{
 
     this.page = page;
  }
+ ////Navigate
+
+ async navigate(url: String)
+ {
+   console.log(`Navigating to ${url}`);
+   await this.page.goto("/elements");
+   await this.page.waitForLoadState('networkidle');
+ }
+
+ async click(locator: Locator){
+   await locator.click()
+
+ }
+
+ async fill(locator:Locator , value: String)
+ {
+await locator.fill('value');
+
+ }
+
+ async textContent(locator:Locator): Promise<string | null>
+ {
+
+   return await locator.textContent();
+ }
+
 
 }
